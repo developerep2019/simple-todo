698d5c84e652b108587d11752a76242a440d8aba
developerep2019 <programmerhasan0@gmail.com>
2021-07-25 22:56:01 +0600

    deleted localstorage palyground foler

--- src/comps/SingleDisplay/SingleDisplay.tsx
+++ src/comps/SingleDisplay/SingleDisplay.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 
-interface Itodo {
-  id: number;
-  isDone: boolean;
-  title: string;
-  desc: string;
-}
-
 const singleDisplay = (todo) => {
   return <div>Hello from single {todo.todo.title}</div>;
 };
